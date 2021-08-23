e59087916fd561fcfd0bb192e81a641848013c71
dusoleil <howcansocksbereal@gmail.com>
2021-08-23 02:52:03 -0400

    add backend ability to get wireless interfaces and switch them in and out of monitor mode

--- api/module.php
+++ api/module.php
@@ -6,7 +6,33 @@ class airmon extends Module
     {
         switch ($this->request->action)
         {
+            case 'getInterfaces':
+                $this->getInterfaces();
+                break;
+            case 'startMonitor':
+                $this->startMonitor();
+                break;
+            case 'stopMonitor':
+                $this->stopMonitor();
+                break;
         }
+    }
+
+    private function getInterfaces()
+    {
+        $interfaces = array();
+        exec("iwconfig 2>/dev/null | grep 'wlan' | awk '{print $1}'",$interfaces);
+        $this->response = array("interfaces" => $interfaces);
+    }
+
+    private function startMonitor()
+    {
+        exec("airmon-ng start ".$this->request->interface);
+    }
+
+    private function stopMonitor()
+    {
+        exec("airmon-ng stop ".$this->request->interface);
     }
 }
 
--- js/module.js
+++ js/module.js
@@ -1,2 +1,57 @@
 registerController('MainController', ['$api', '$scope', function($api, $scope) {
+    $scope.interfaces = [];
+    $scope.selectedInterface = "";
+    $scope.modinterface = false;
+
+    $scope.getInterfaces = (function()
+        {
+            $api.request(
+                {
+                    module: 'airmon',
+                    action: 'getInterfaces'
+                },
+                function(response)
+                {
+                    $scope.interfaces = response.interfaces;
+                    $scope.selectedInterface = $scope.interfaces[0];
+                    console.log(response)
+                }
+            );
+        });
+
+    $scope.startMonitor = (function()
+        {
+            $scope.modinterface = true;
+            $api.request(
+                {
+                    module: 'airmon',
+                    action: 'startMonitor',
+                    interface: $scope.selectedInterface
+                },
+                function(response)
+                {
+                    $scope.getInterfaces();
+                    $scope.modinterface = false;
+                }
+            );
+        });
+
+    $scope.stopMonitor = (function()
+        {
+            $scope.modinterface = true;
+            $api.request(
+                {
+                    module: 'airmon',
+                    action: 'stopMonitor',
+                    interface: $scope.selectedInterface
+                },
+                function(response)
+                {
+                    $scope.getInterfaces();
+                    $scope.modinterface = false;
+                }
+            );
+        });
+
+    $scope.getInterfaces();
 }]);
